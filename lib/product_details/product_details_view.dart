636364268365be9d94daa38a87207319fa88e863
Tayeb Ahmed <tayebahmed97@gmail.com>
2026-05-02 22:26:09 +0600

    Add shimmer loading effect

--- lib/product_details/product_details_view.dart
+++ lib/product_details/product_details_view.dart
@@ -9,6 +9,7 @@ import 'package:learn_provider/product_details/widgets/product_header_widget.dar
 import 'package:learn_provider/product_details/widgets/product_info_widget.dart';
 import 'package:learn_provider/product_details/widgets/reviews_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ProductDetailsView extends StatelessWidget {
   const ProductDetailsView({super.key});
@@ -16,12 +17,16 @@ class ProductDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Step 1: Get productId from route
-    final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     final productId = args["id"] ?? 0;
 
     // Step 2: Fetch data once after frame renders
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<ProductDetailsController>(context, listen: false).fetchProductDetails(id: productId);
+      Provider.of<ProductDetailsController>(
+        context,
+        listen: false,
+      ).fetchProductDetails(id: productId);
     });
 
     return Consumer<ProductDetailsController>(
@@ -51,38 +56,53 @@ class ProductDetailsView extends StatelessWidget {
                         brand: controller.productDetails.brand,
                         sku: controller.productDetails.sku,
                         price: controller.productDetails.price,
-                        discountPercentage: controller.productDetails.discountPercentage,
+                        discountPercentage:
+                            controller.productDetails.discountPercentage,
                         rating: controller.productDetails.rating,
                         tags: controller.productDetails.tags,
-                        availabilityStatus: controller.productDetails.availabilityStatus,
+                        availabilityStatus:
+                            controller.productDetails.availabilityStatus,
                       ),
-                      ProductDescriptionWidget(description: controller.productDetails.description),
+                      ProductDescriptionWidget(
+                        description: controller.productDetails.description,
+                      ),
                       ProductInfoWidget(
                         category: controller.productDetails.category,
                         weight: controller.productDetails.weight,
                         stock: controller.productDetails.stock,
-                        minimumOrderQuantity: controller.productDetails.minimumOrderQuantity,
+                        minimumOrderQuantity:
+                            controller.productDetails.minimumOrderQuantity,
                         dimensions: controller.productDetails.dimensions,
                       ),
                       PoliciesWidget(
-                        warrantyInformation: controller.productDetails.warrantyInformation,
-                        shippingInformation: controller.productDetails.shippingInformation,
+                        warrantyInformation:
+                            controller.productDetails.warrantyInformation,
+                        shippingInformation:
+                            controller.productDetails.shippingInformation,
                         returnPolicy: controller.productDetails.returnPolicy,
                       ),
                       MetaWidget(meta: controller.productDetails.meta),
                       ReviewsWidget(reviews: controller.productDetails.reviews),
                       const SizedBox(height: 24),
                       ActionButtonsWidget(
-                        onAddToCart: (){
-
-                        },
-                        onAddToWishlist: (){},
+                        onAddToCart: () {},
+                        onAddToWishlist: () {},
                       ),
                     ],
                   ),
                 ),
               )
-            : Center(child: CircularProgressIndicator()),
+            : Center(
+                child: SizedBox(
+                  width: 200.0,
+                  height: 100.0,
+                  child: Shimmer.fromColors(
+                    baseColor: Colors.red,
+                    highlightColor: Colors.yellow,
+                    child: Center(child: Icon(Icons.front_loader,size: 100,)),
+                  ),
+                ),
+              ),
       ),
     );
   }
